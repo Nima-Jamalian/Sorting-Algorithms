9e4cfbb531fb6fe4a3f6079057f30f2a3745775a
Nima Jamalian <nima.jamalian80@gmail.com>
2021-11-26 13:16:07 +0000

    Hybrid Sort

--- AlgorithmAndDataStructure/Algorithms/src/App.java
+++ AlgorithmAndDataStructure/Algorithms/src/App.java
@@ -4,27 +4,33 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         StartPrograme();
-
     }
 
     public static void StartPrograme(){
-        int[] input = {7,6,4,3};
-        BubbleSort bubbleSort = new BubbleSort();
-        SelectionSort selectionSort = new SelectionSort();
-
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("Start Programme:");
         System.out.println("Press 1 for Bubble Sort:");
         System.out.println("Press 2 for Selection Sort:");
+        System.out.println("Press 3 for Hybrid Sort:");
+        Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
+        int[] input = {9,8,7,6};
         switch(userInput){
             case "1":
-            System.out.println(Arrays.toString(bubbleSort.Start(input)));
+            BubbleSort bubbleSort = new BubbleSort();
+            System.out.println("Bubble Sort: " + Arrays.toString(bubbleSort.Start(input)));
+            System.out.println("");
             StartPrograme();
             break;
             case "2":
-            System.out.println(Arrays.toString(selectionSort.Start(input)));
+            SelectionSort selectionSort = new SelectionSort();
+            System.out.println("Selection Sort: " + Arrays.toString(selectionSort.Start(input)));
+            System.out.println("");
+            StartPrograme();
+            break;
+            case "3":
+            HybridSort hybridSort = new HybridSort();
+            System.out.println("Hybrid Sort: " + Arrays.toString(hybridSort.Start(input)));
+            System.out.println("");
             StartPrograme();
             break;
             default:
--- AlgorithmAndDataStructure/Algorithms/src/BubbleSort.java
+++ AlgorithmAndDataStructure/Algorithms/src/BubbleSort.java
@@ -1,18 +1,19 @@
 import java.util.Arrays;
 public class BubbleSort{
     public int [] Start(int[] array) {
+        System.out.println("Input: " + Arrays.toString(array));
         boolean toSwap = true;
         int temp;
-    
         while(toSwap){
-            //System.out.println(Arrays.toString(array));
             toSwap = false;
             for(int i = 0;i < array.length-1; i++){              
                 if(array[i] > array[i+1]){
+
                     temp = array[i];
                     array[i] = array[i+1];
                     array[i+1] = temp;                   
                     toSwap = true;
+                    System.out.println(Arrays.toString(array));
                 }
             }
         }
