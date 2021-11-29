ef00420abadcec05b67c09c399323261110af634
Nima Jamalian <nima.jamalian80@gmail.com>
2021-11-29 21:30:23 +0000

    update

--- C++/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-    // int max = max_element(begin(array),end(array));
-    // int min = min_element(begin(array),end(array));
-    // cout << " Maz = " + max << endl;
-    // cout << " Min = " + min << endl;
